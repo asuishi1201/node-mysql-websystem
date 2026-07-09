781a879d66fcbd223c7afc6fbc404c50a35636b4
asuishi1201 <j2303@Matsue-ct.ac.jp>
2026-07-09 06:17:15 +0000

    bcryptでパスワードをハッシュ化しよう

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
+const bcrypt = require("bcrypt");
 
 
 router.get('/', function (req, res, next) {
@@ -14,7 +15,7 @@ const isAuth = Boolean(userId);
 
 router.post('/', function (req, res, next) {
   const userId = req.session.userid;
-const isAuth = Boolean(userId);
+  const isAuth = Boolean(userId);
   const username = req.body.username;
   const password = req.body.password;
   const repassword = req.body.repassword;
@@ -22,16 +23,18 @@ const isAuth = Boolean(userId);
   knex("users")
     .where({name: username})
     .select("*")
-    .then(function (result) {
+    .then(async function (result) {
       if (result.length !== 0) {
         res.render("signup", {
           title: "Sign up",
-          isAuth: isAuth,
           errorMessage: ["このユーザ名は既に使われています"],
-        }) 
+          isAuth: isAuth,
+        })
       } else if (password === repassword) {
+        const hashedPassword = await bcrypt.hash(password, 10);
+  
         knex("users")
-          .insert({name: username, password: password})
+          .insert({name: username, password: hashedPassword})
           .then(function () {
             res.redirect("/");
           })
@@ -39,15 +42,15 @@ const isAuth = Boolean(userId);
             console.error(err);
             res.render("signup", {
               title: "Sign up",
-              isAuth: isAuth,
               errorMessage: [err.sqlMessage],
+              isAuth: isAuth,
             });
           });
       } else {
         res.render("signup", {
           title: "Sign up",
-          isAuth: isAuth,
           errorMessage: ["パスワードが一致しません"],
+          isAuth: isAuth,
         });
       }
     })
@@ -55,8 +58,8 @@ const isAuth = Boolean(userId);
       console.error(err);
       res.render("signup", {
         title: "Sign up",
-        isAuth: isAuth,
         errorMessage: [err.sqlMessage],
+        isAuth: isAuth,
       });
     });
 });
